263ffe4edb6333cfc5eb9490a39d502729f27692
Eli Fonseca <Eli Fonseca>
2016-02-23 11:23:16 -0800

    updating the look of the page

--- onOffScript.php
+++ onOffScript.php
@@ -11,43 +11,61 @@ Version: 1.0
 <!--
 imports the ActiveX object used for Wol in the applicaiton
 -->
-<object data="http://www.activexperts.com/files/network-component/cab/4.4/axnetwork32.cab" style="visibility: hidden;">
+<!-- <object data="http://www.activexperts.com/files/network-component/cab/4.4/axnetwork32.cab" style="visibility: hidden;">
 	<param name="classid" value="CLSID:B52B14BA-244B-4006-86E0-2923CB69D881">
-</object>
+</object> -->
 
 
 <html>
-	<body>
-		<button onclick="turnOn()">On</button>
-		</br>
-		
-		<form id="input" method="GET"> 
-			<input type="submit" value="Submit" /> <br/> 
-		</form>
+	<head>
+	<style type="text/css">
+		.onButton {
+			width: 40%;
+			height: 25%;
+			position:absolute;
+			left: 5%;
+			background-color:#76EE00;
 
-		<?php
-			if ($_SERVER["REQUEST_METHOD"] == "GET") {
-				$command = escapeshellcmd('py powerOff.py');
-         		$powerOffOutput = shell_exec($command);
-        	}
-		?>
+		}
+		.offButton {
+			width: 40%;
+			height: 25%;
+			position:absolute;
+    		left: 55%;
+			background-color:#FF0000;
+		}
+	</style>
+		<body>
+			<button class="onButton" onclick="turnOn()">On</button>	
+			
+			<form id="input" method="GET"> 
+				<input type="submit" value="Off" class="offButton"/>
+			</form>
 
-		<script>
-		//initalize stuff used for WoL
-			var objWOL = new ActiveXObject("AxNetwork.WOL"); //object used for WoL
-			var MAC = "44-94-FC-6E-0C-C8"; //MAC address of NAS
+			<?php
+				if ($_SERVER["REQUEST_METHOD"] == "GET") {
+					$command = escapeshellcmd('py powerOff.py');
+	         		$powerOffOutput = shell_exec($command);
+	        	}
+			?>
 
-			function turnOn() {
-				var numLastError = 0;
-  				var strLastError = "";
-				objWOL.Clear();
+			<script>
+			//initalize stuff used for WoL
+				var objWOL = new ActiveXObject("AxNetwork.WOL"); //object used for WoL
+				var MAC = "44-94-FC-6E-0C-C8"; //MAC address of NAS
 
-				objWOL.WakeUp(MAC);
+				function turnOn() {
+					var numLastError = 0;
+	  				var strLastError = "";
+					objWOL.Clear();
 
-				numLastError =  objWOL.LastError;
-  				strLastError =  objWOL.GetErrorDescription(numLastError);
-  				console.log(numLastError + " : " + strLastError);  
-			}
-		</script>
-	</body>
+					objWOL.WakeUp(MAC);
+
+					numLastError =  objWOL.LastError;
+	  				strLastError =  objWOL.GetErrorDescription(numLastError);
+	  				console.log(numLastError + " : " + strLastError);  
+				}
+			</script>
+		</body>
+	</head>
 </html>
